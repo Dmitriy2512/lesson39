26ff41447d2bd51fe92f117c2f65e5c6514ee3a7
Dmitriy2512 <dmitriyisaev223@gmail.com>
2022-02-17 23:29:32 +0300

    lesson39

---
+++ main.js
@@ -0,0 +1,92 @@
+// Функция
+// Процедура
+
+// function name(params) {
+//     Инструкция
+// }
+
+// function sendMessage() {
+//     alert("Привет")
+// }
+// sendMessage()
+
+// let name = prompt("Введите своё имя ") // Глобальные переменные
+// let age = +prompt("Введите свой возраст") // Глобальные переменные
+// Java()
+// function Java() {
+//     let name = "Дима" // Локальные переменные
+//     let age = 15 // Локальные переменные
+//     alert(`Привет, тебя зовут ${name} . И тебе ${age} лет.`)
+// }
+// alert(`Привет, тебя зовут ${name} . И тебе ${age} лет.`)
+
+// function sendMessage(name, age) {
+//     let a = `Привет, тебя зовут ${name} . И тебе ${age} лет.`
+//     alert(a)
+// }
+// let a = prompt("Как тебя зовут ")
+// let b = +prompt("Какой твой возраст ")
+// sendMessage(a, b)
+
+// Функция
+// function name(params) {
+//     Инструкция
+//     return результат
+// }
+// let a = name()
+// alert(a)
+
+// function sendMessage(name, age) {
+//     let Message = `Привет, тебя зовут ${name} . И тебе ${age} лет.`
+//     return Message
+// }
+// let a = prompt("Как тебя зовут ")
+// let b = +prompt("Какой твой возраст ")
+// sendMessage(a, b)
+// let result = sendMessage(a, b)
+// alert(result)
+
+// DZ
+
+// 8.1
+// let a = prompt("Как тебя зовут? ")
+// let b = prompt("Сколько тебе лет? ")
+// function display_message(a, b){
+//     let Java = (`Тебя зовут ${a}, ты такой взрослый ${b}`)
+//     return Java
+// }
+// let Javaa = display_message(a, b)
+// alert(Javaa)
+
+// 8.2
+// let title = prompt("Введите название вашей любимой книги")
+// function favorite_book(){
+//     alert("Моя любимая книга " + title)
+// }
+// favorite_book()
+
+// 8.3
+// let a = prompt(`text`)
+// let b = prompt(`size`)
+// function make_shirt(a, b) {
+//     let Java = (`text = ${a}, size = ${b}`)
+//     return Java
+// }
+// let Javaa = make_shirt(a, b)
+// alert(Javaa)
+
+// 8.4
+// let size = "L"
+// let text = "I love JavaScript"
+// function make_shirt(size, text){
+//     alert(`Твой размер ${size}, твой текст ${text}`)
+// }
+// make_shirt(size, text)
+
+// 8.5
+// let country = prompt("В какой стране ты живете?")
+// let city = prompt("В каком городе ты живете? ")
+// function discribe_city() {
+//     alert(`Ты живешь в ${city}, город находиться в ${country}`)
+// }
+// discribe_city()
